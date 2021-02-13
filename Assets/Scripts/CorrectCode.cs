2c1c0e36f5672098355e0c266fea575917436dad
Sergio1811 <serroddev@gmail.com>
2021-02-14 00:03:32 +0100

    All scenes ready

---
+++ Assets/Scripts/CorrectCode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CorrectCode : MonoBehaviour
+{
+    public string codeToMatch;
+
+  public void codeRight(TextMeshProUGUI l_CodeFilled)
+    {
+        string codeFilled = l_CodeFilled.text;
+        codeFilled = codeFilled.Substring(0, codeFilled.Length - 1);
+        if (codeToMatch==codeFilled)
+        {
+            //dothings
+            print("funiono");
+        }
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject[] scenes;
+    public void DeactivateAllScenes()
+    {
+        foreach (var item in scenes)
+        {
+            item.SetActive(false);
+        }
+    }
+}
