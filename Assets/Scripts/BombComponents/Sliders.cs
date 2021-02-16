62944a8ad84f07e07afade1a0ff040f40975e666
Sergio1811 <serroddev@gmail.com>
2021-02-16 22:16:03 +0100

    fonts ui y slider

--- Assets/Scripts/BombComponents/Sliders.cs
+++ Assets/Scripts/BombComponents/Sliders.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Sliders : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int currentPos;
+    public int correctPos;
+    public int howManyPos;
+
+    public Bomb m_Bomb;
+    public int ID;
     void Start()
     {
-        
+        this.transform.position = new Vector3(currentPos * 0.5f, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            RectTransform invPanel = transform as RectTransform;
+
+            if (!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject == this.gameObject)
+                    {
+                        if (Input.GetAxis("Mouse X") < 0)
+                        {
+                            //Code for action on mouse moving left
+                            print("Mouse moved left");
+                            if(currentPos>1)
+                            currentPos--;
+                        }
+                        if (Input.GetAxis("Mouse X") > 0)
+                        {
+                            //Code for action on mouse moving right
+                            print("Mouse moved right");
+                            if(currentPos<howManyPos)
+                            currentPos++;
+                        }
+                        if (currentPos == correctPos)
+                        {
+                            m_Bomb.BombDeactivated(ID);
+                        }
+                        else
+                        {
+                            m_Bomb.correctComponets[ID] = false;
+                        }
+                        this.transform.position = new Vector3(currentPos * 0.5f, 0, 0);
+
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/LoadingRT.cs
+++ Assets/Scripts/LoadingRT.cs
@@ -7,20 +7,32 @@ public class LoadingRT : MonoBehaviour
     public float timeToLoad;
     float currentTimeLoading;
     public Image imageToLoad;
+    public float range;
+    int currentMoment = 1;
     void Start()
     {
-        
+        StartCoroutine(sumandoBarra());
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTimeLoading += Time.deltaTime;
+       /* currentTimeLoading += Time.deltaTime;
         imageToLoad.fillAmount = currentTimeLoading / timeToLoad;
         if(currentTimeLoading>=timeToLoad)
         {
             //do things
             print("dothings");
-        }
+        }*/
+    }
+
+    public IEnumerator sumandoBarra()
+    {
+        yield return new WaitForSeconds(Random.Range(0.5f,2));
+        imageToLoad.fillAmount = range * currentMoment;
+        currentMoment++;
+
+        if(imageToLoad.fillAmount<1)
+        StartCoroutine(sumandoBarra());
     }
 }
